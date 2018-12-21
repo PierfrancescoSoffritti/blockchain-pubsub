39980b60979754bd804b4d94197be8fe6764957f
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2018-12-21 17:21:06 +0100

    chaincode refactoring

--- chaincode/IOchannel/node/IOchannel.js
+++ chaincode/IOchannel/node/IOchannel.js
@@ -1,118 +1,126 @@
-/*
-# Copyright IBM Corp. All Rights Reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-*/
-
 'use strict';
 const shim = require('fabric-shim');
 const util = require('util');
 
 let Chaincode = class {
 
-  // The Init method is called when the Smart Contract 'IOchannel' is instantiated by the blockchain network
-  // Best practice is to have any Ledger initialization in separate function -- see initLedger()
+  /**
+   * Method called when the chaincode is instantiated by the blockchain network. 
+   * Best practice is to have any Ledger initialization in separate function -- see initLedger()
+   */
   async Init(stub) {
-    console.info('=========== Instantiated IOchannel chaincode ===========');
+    console.info('=========== start Init ===========');
     return shim.success();
   }
 
-  // The Invoke method is called as a result of an application request to run the Smart Contract
-  // 'IOchannel'. The calling application program has also specified the particular smart contract
-  // function to be called, with arguments
+  /**
+   * Method called as a result of an application request to run the chaincode
+   */
   async Invoke(stub) {
-    let ret = stub.getFunctionAndParameters();
-    console.info(ret);
+    console.info('=========== start Invoke ===========');
+    const invocationArguments = stub.getFunctionAndParameters();
+    console.info(invocationArguments);
 
-    let method = this[ret.fcn];
-    if (!method) {
-      console.error('no function of name:' + ret.fcn + ' found');
-      throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+    const requestedFunction = this[invocationArguments.fcn];
+    if (!requestedFunction) {
+      console.error(`no function with name: ${invocationArguments.fcn} found`);
+      throw new Error(`Received unknown function ${invocationArguments.fcn} invocation`);
     }
+    
     try {
-      let payload = await method(stub, ret.params);
+      const payload = await requestedFunction(stub, invocationArguments.params);
       return shim.success(payload);
-    } catch (err) {
-      console.log(err);
-      return shim.error(err);
+    } catch (error) {
+      console.log(error);
+      return shim.error(error);
     }
-  }
-
-  async queryMessage(stub, args) {
-    if (args.length != 1) {
-      throw new Error('Incorrect number of arguments. Expecting MessageId ex: MSG0');
-    }
-    let messageId = args[0];
-
-    let messageAsBytes = await stub.getState(messageId); //get the message from chaincode state
-    if (!messageAsBytes || messageAsBytes.toString().length <= 0) {
-      throw new Error(messageId + ' does not exist: ');
-    }
-    console.log(messageAsBytes.toString());
-    return messageAsBytes;
   }
 
   async initLedger(stub, args) {
-    console.info('============= START : Initialize Ledger ===========');
+    console.info('============= start initLedger ===========');
     
-    let messages = [];
-    messages.push({
-      senderId: 'testSender',
-      content: 'This is a test message'
-    });
+    const messages = [
+      {
+        senderId: 'testSender',
+        content: 'This is a test message'
+      }
+    ];
 
-    for (let i = 0; i < messages.length; i++) {
-      await stub.putState('MSG' +i, Buffer.from(JSON.stringify(messages[i])));
-      console.info('Added <--> ', messages[i]);
+    for(let i=0; i<messages.length; i++) {
+      await stub.putState(`MSG${i}`, Buffer.from(JSON.stringify(messages[i])));
+      console.info(`Added:  ${messages[i]}`);
     }
 
-    console.info('============= END : Initialize Ledger ===========');
+    console.info('============= finish initLedger ===========');
+  }
+
+  async queryMessage(stub, args) {
+    console.info('============= start queryMessage ===========');
+    if (args.length != 1)
+      throw new Error('Incorrect number of arguments. Expected messageId is missing');
+    
+    const messageId = args[0];
+
+    const messageAsBytes = await stub.getState(messageId);
+    if(!messageAsBytes || messageAsBytes.toString().length <= 0)
+      throw new Error(`${messageId} does not exist.`);
+
+    console.log(messageAsBytes.toString());
+    console.info('============= finish queryMessage ===========');
+
+    return messageAsBytes;
   }
 
   async putMessage(stub, args) {
-    console.info('============= START : putMessage ===========');
-
+    console.info('============= start putMessage ===========');
     if (args.length != 3)
-      throw new Error('Incorrect number of arguments. Expecting 3');
+      throw new Error('Incorrect number of arguments. Expecting 3 arguments');
 
-    var message = {
+    const messageId = args[0];
+    const message = {
       senderId: args[1],
       content: args[2]
     };
 
-    await stub.putState(args[0], Buffer.from(JSON.stringify(message)));
+    await stub.putState(messageId, Buffer.from(JSON.stringify(message)));
 
-    console.info('============= END : putMessage ===========');
+    console.info('============= finish putMessage ===========');
   }
 
   async queryAllMessages(stub, args) {
+    console.info('============= start queryAllMessages ===========');
 
-    let startKey = 'MSG0';
-    let endKey = 'MSG999';
+    const startKey = 'MSG0';
+    const endKey = 'MSG999';
 
-    let iterator = await stub.getStateByRange(startKey, endKey);
+    const iterator = await stub.getStateByRange(startKey, endKey);
 
-    let allResults = [];
+    const allResults = [];
     while (true) {
-      let res = await iterator.next();
+      const nextValue = await iterator.next();
 
-      if (res.value && res.value.value.toString()) {
-        let jsonRes = {};
-        console.log(res.value.value.toString('utf8'));
+      if (nextValue.value && nextValue.value.value.toString()) {
+        const jsonRes = {};
+        console.log(nextValue.value.value.toString('utf8'));
 
-        jsonRes.Key = res.value.key;
+        jsonRes.Key = nextValue.value.key;
         try {
-          jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+          jsonRes.Record = JSON.parse(nextValue.value.value.toString('utf8'));
         } catch (err) {
           console.log(err);
-          jsonRes.Record = res.value.value.toString('utf8');
+          jsonRes.Record = nextValue.value.value.toString('utf8');
         }
         allResults.push(jsonRes);
       }
-      if (res.done) {
+      
+      if (nextValue.done) {
         console.log('end of data');
+        
         await iterator.close();
+
         console.info(allResults);
+        console.info('============= stop queryAllMessages ===========');
+
         return Buffer.from(JSON.stringify(allResults));
       }
     }
