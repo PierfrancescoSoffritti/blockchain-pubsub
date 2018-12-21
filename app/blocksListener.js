1d878c7d50cdf69e3ce8e2d8fe2d2ed295594bf4
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2018-12-21 10:58:57 +0100

    adds basic code to listen for new blocks on blockchain

---
+++ app/blocksListener.js
@@ -0,0 +1,80 @@
+'use strict';
+/*
+* Copyright IBM Corp All Rights Reserved
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+/*
+ * Chaincode Invoke
+ */
+
+var Fabric_Client = require('fabric-client');
+var path = require('path');
+var util = require('util');
+var os = require('os');
+
+//
+var fabric_client = new Fabric_Client();
+
+// setup the fabric network
+var channel = fabric_client.newChannel('mychannel');
+var peer = fabric_client.newPeer('grpc://localhost:7051');
+channel.addPeer(peer);
+var order = fabric_client.newOrderer('grpc://localhost:7050')
+channel.addOrderer(order);
+
+//
+var member_user = null;
+var store_path = path.join(__dirname, 'hfc-key-store');
+console.log('Store path:'+store_path);
+var tx_id = null;
+
+
+// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+Fabric_Client.newDefaultKeyValueStore({ path: store_path }).then((state_store) => {
+	// assign the store to the fabric client
+	fabric_client.setStateStore(state_store);
+	var crypto_suite = Fabric_Client.newCryptoSuite();
+	// use the same location for the state store (where the users' certificate are kept)
+	// and the crypto store (where the users' keys are kept)
+	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
+	crypto_suite.setCryptoKeyStore(crypto_store);
+	fabric_client.setCryptoSuite(crypto_suite);
+
+	// get the enrolled user from persistence, this user will sign all requests
+	return fabric_client.getUserContext('user1', true);
+}).then((user_from_store) => {
+	if (user_from_store && user_from_store.isEnrolled()) {
+		console.log('Successfully loaded user1 from persistence');
+		member_user = user_from_store;
+	} else {
+		throw new Error('Failed to get user1.... run registerUser.js');
+    }
+    
+
+    // get an eventhub once the fabric client has a user assigned. The user
+    // is required bacause the event registration must be signed
+    let event_hub = channel.newChannelEventHub(peer);
+
+    let txPromise = new Promise((resolve, reject) => {
+
+        const transaction_id_string = "*"
+
+        let handle = setTimeout(() => {
+            event_hub.disconnect();
+            resolve({event_status : 'TIMEOUT'});
+        }, 30000);
+
+        console.log("\nregisterBlockEvent")
+
+        let block_reg = event_hub.registerBlockEvent((block) => {
+            console.log('Successfully received the block event');
+        }, (error)=> {
+            console.log('Failed to receive the block event ::'+error);
+            event_hub.disconnect();
+        });
+        event_hub.connect();
+
+    });
+
+})
