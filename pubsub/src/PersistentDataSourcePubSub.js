85c7d4729e581cf8bececf71d339f25b4415aa8d
PierfrancescoSoffritti <soffritti.pierfrancesco@gmail.com>
2018-12-30 15:31:54 +0100

    refactoring and tests

--- pubsub/src/PersistentDataSourcePubSub.js
+++ pubsub/src/PersistentDataSourcePubSub.js
@@ -3,12 +3,11 @@ const MessageIdGenerator = require('./MessageIdGenerator')
 function PersistentDataSourcePubSub(hubId, persistentDataSource) {
     let onNewMessage = () => {}
 
-    let lastSentMessageId = "MSG0"
-    let lastQueryMessageId = "MSG0"
+    let lastSentMessageId = "MSG-1"
+    let lastQueryMessageId = "MSG-1"
 
     this.sendMessage = async function(message) {        
         const newMessageId = MessageIdGenerator.nextMessageId(lastSentMessageId)
-        console.log("[INDEX] sendMessage, id: " +newMessageId)
         lastSentMessageId = newMessageId;
 
         await persistentDataSource.persist( { id: `${newMessageId}-${hubId}`, content: message } )
@@ -17,7 +16,7 @@ function PersistentDataSourcePubSub(hubId, persistentDataSource) {
     this.onNewMessage = async function(messageListener) {
         onNewMessage = messageListener
         
-        const listenerConnection = await persistentDataSource.onData(onNewPersistentDataAvailable)
+        const listenerConnection = await persistentDataSource.onDataPersisted(onNewPersistentDataAvailable)
         return listenerConnection
     }
 
@@ -33,23 +32,26 @@ function PersistentDataSourcePubSub(hubId, persistentDataSource) {
        
         const queryUpperBound = MessageIdGenerator.addOffset(queryLowerBound, 999)
 
-        console.log("[INDEX] queryNewMessages lower bound: " +queryLowerBound)
-        console.log("[INDEX] queryNewMessages upper bound: " +queryUpperBound +"\n")
+        // console.log("[INDEX] queryNewMessages lower bound: " +queryLowerBound)
+        // console.log("[INDEX] queryNewMessages upper bound: " +queryUpperBound +"\n")
 
-        const stringResponse = await persistentDataSource.queryRange(queryLowerBound, queryUpperBound)
-        const response = JSON.parse(stringResponse)
+        const response = await persistentDataSource.queryRange(queryLowerBound, queryUpperBound)
+        // const response = JSON.parse(stringResponse)
 
-        console.log(`[INDEX] response.length: ${response.length}`)
+        // console.log(`[INDEX] response.length: ${response.length}`)
         if(response.length === 0)
             return []
 
+        // const messages = response
+        //     .sort( (r1, r2) => MessageIdGenerator.compare(r1["Key"].split('-')[0], r2["Key"].split('-')[0]))
+        //     .map( r => { console.log("[INDEX] queryNewMessages key: " +r["Key"]); return r } )
+        //     .map( r => r["Record"] )
         const messages = response
-            .sort( (r1, r2) => MessageIdGenerator.compare(r1["Key"].split('-')[0], r2["Key"].split('-')[0]))
-            .map( r => { console.log("[INDEX] queryNewMessages key: " +r["Key"]); return r } )
-            .map( r => r["Record"] )
+            .sort( (r1, r2) => MessageIdGenerator.compare(r1.id.split('-')[0], r2.id.split('-')[0]))
+            .map( object => object.content )
 
-        const lastKey = response[response.length-1]["Key"]
-        const keyMessageId = lastKey.split('-')[0]
+        const lastId = response[response.length-1].id
+        const keyMessageId = lastId.split('-')[0]
         lastQueryMessageId = MessageIdGenerator.nextMessageId(keyMessageId)
        
         return messages
--- pubsub/tests/persistentDataSourcePubSub_test.js
+++ pubsub/tests/persistentDataSourcePubSub_test.js
@@ -1,18 +1,131 @@
 const expect = require('chai').expect
+const sinon = require('sinon')
 const PersistentDataSourcePubSub = require('../src/PersistentDataSourcePubSub')
 const MockPersistentDataSource = require('./mocks/MockPersistentDataSource')
+const { wait } = require('./utils')
 
 describe('sendMessage', () => {
     it('base test', async () => {
         
         // 1. ARRANGE
+        const pubsubId = 1
         const persistentData = new MockPersistentDataSource()
-        const pubsub = new PersistentDataSourcePubSub(1, persistentData)
+        const pubsub = new PersistentDataSourcePubSub(pubsubId, persistentData)
 
         // 2. ACT
         await pubsub.sendMessage("message #1")
 
         // 3. ASSERT
         expect(persistentData.getDataArray().length).to.be.equal(1)
+        expect(persistentData.getDataArray()[0].id).to.be.equal(`MSG0-${pubsubId}`)
+        expect(persistentData.getDataArray()[0].content).to.be.equal(`message #1`)
+    })
+
+    it('multiple messages test', async () => {
+        
+        // 1. ARRANGE
+        const pubsubId = 1
+        const persistentData = new MockPersistentDataSource()
+        const pubsub = new PersistentDataSourcePubSub(pubsubId, persistentData)
+
+        // 2. ACT
+        await pubsub.sendMessage("message #1")
+        await pubsub.sendMessage("message #2")
+        await pubsub.sendMessage("message #3")
+
+        // 3. ASSERT
+        expect(persistentData.getDataArray().length).to.be.equal(3)
+        expect(persistentData.getDataArray()[0].id).to.be.equal(`MSG0-${pubsubId}`)
+        expect(persistentData.getDataArray()[0].content).to.be.equal(`message #1`)
+        expect(persistentData.getDataArray()[1].id).to.be.equal(`MSG1-${pubsubId}`)
+        expect(persistentData.getDataArray()[1].content).to.be.equal(`message #2`)
+        expect(persistentData.getDataArray()[2].id).to.be.equal(`MSG2-${pubsubId}`)
+        expect(persistentData.getDataArray()[2].content).to.be.equal(`message #3`)
+    })
+
+    it('multiple sources test', async () => {
+        
+        // 1. ARRANGE
+        const pubsubId1 = 1
+        const pubsubId2 = 2
+        const persistentData = new MockPersistentDataSource()
+        const pubsub1 = new PersistentDataSourcePubSub(pubsubId1, persistentData)
+        const pubsub2 = new PersistentDataSourcePubSub(pubsubId2, persistentData)
+
+        // 2. ACT
+        await pubsub1.sendMessage("message #1")
+        await pubsub2.sendMessage("message #2")
+        await pubsub1.sendMessage("message #3")
+
+        // 3. ASSERT
+        expect(persistentData.getDataArray().length).to.be.equal(3)
+        expect(persistentData.getDataArray()[0].id).to.be.equal(`MSG0-${pubsubId1}`)
+        expect(persistentData.getDataArray()[0].content).to.be.equal(`message #1`)
+        expect(persistentData.getDataArray()[1].id).to.be.equal(`MSG0-${pubsubId2}`)
+        expect(persistentData.getDataArray()[1].content).to.be.equal(`message #2`)
+        expect(persistentData.getDataArray()[2].id).to.be.equal(`MSG1-${pubsubId1}`)
+        expect(persistentData.getDataArray()[2].content).to.be.equal(`message #3`)
+    })
+})
+
+describe('onNewMessage', () => {
+    it('base test', async () => {
+        // 1. ARRANGE
+        const pubsubId = 1
+        const persistentData = new MockPersistentDataSource()
+        const pubsub = new PersistentDataSourcePubSub(pubsubId, persistentData)
+
+        const onNewMessage = sinon.fake()
+        pubsub.onNewMessage(onNewMessage)
+
+        // 2. ACT
+        await pubsub.sendMessage("message #1")
+
+        // 3. ASSERT
+        await wait(6)
+        expect(onNewMessage.calledOnce).to.be.true;
+    })
+
+    it('correct message is passed as argument test', async () => {
+        // 1. ARRANGE
+        const pubsubId = 1
+        const persistentData = new MockPersistentDataSource()
+        const pubsub = new PersistentDataSourcePubSub(pubsubId, persistentData)
+
+        const callback = sinon.stub()
+        pubsub.onNewMessage(callback)
+
+        // 2. ACT
+        await pubsub.sendMessage("message #1")
+
+        // 3. ASSERT
+        await wait(6)
+        expect(callback.calledWith("message #1")).to.be.true
+    })
+
+    it('data is sorted test', async () => {
+        // 1. ARRANGE
+        const pubsubId1 = 1
+        const pubsubId2 = 2
+        const persistentData = new MockPersistentDataSource()
+        const pubsub1 = new PersistentDataSourcePubSub(pubsubId1, persistentData)
+        const pubsub2 = new PersistentDataSourcePubSub(pubsubId2, persistentData)
+
+        const callback = sinon.stub()
+        callback.withArgs("message #1").returns(1)
+        callback.withArgs("message #2").returns(2)
+        callback.withArgs("message #3").returns(3)
+        callback.returns(0)
+
+        pubsub1.onNewMessage(callback)
+
+        // 2. ACT
+        await pubsub1.sendMessage("message #1")
+        await pubsub1.sendMessage("message #3")
+        await pubsub2.sendMessage("message #2")
+
+        // 3. ASSERT
+        await wait(6)
+        expect(callback.returnValues).to.have.ordered.members([1, 2, 3]);
     })
 })
